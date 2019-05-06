1eabefde72478a355c7776a1a41f2cd16fea7f40
Eduardo Faneli <eduardofaneli2@gmail.com>
2019-05-06 11:22:56 -0400

    refatorando
    refatorando código, implementando blueprints

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify
+from flask import Flask#, jsonify
 from flask_cors import CORS
-from boss_table import BossTable
+
+from boss_table.views import boss_table
 
 app = Flask(__name__)
-
 cors = CORS(app, resources = {r"/*": {"origins": "*"}})
-
-@app.route('/', methods=["GET"])
-def boss_table():          
-    return BossTable().get_table()
-
-@app.route('/<day>', methods=['GET'])
-def boss_by_day(day):
-    return BossTable().get_by_day(day)
-
-@app.route('/today', methods=['GET'])
-def boss_today():
-    return BossTable().get_today()    
+app.register_blueprint(boss_table)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- routes/boss.py
+++ routes/boss.py
@@ -1,33 +1,36 @@
 from flask import jsonify
 from datetime import date
 import json, os
+from pathlib import Path
 
-class BossTable(object):
-    def __init__(self):
-        pass
+class BossRoutes(object):
+    def __init__(self):        
+        self.boss_json = self.load_json().get("days")
+        self.full_json = self.load_json()
 
     def load_json(self):
-        try:
-            file = open('./table_new.json', encoding='UTF-8').read()
+        try:                                
+            file = open('./routes/table_boss.json', encoding='UTF-8').read()                    
             return json.loads(file)            
-        except Exception as ex:
+        except Exception as ex:                    
             return jsonify({'success': False, 'message': ex.args}), 401    
 
     def get_table(self):
-        try:
-            return jsonify(self.load_json()), 200
+        try:            
+            return jsonify(self.full_json), 200
         except Exception as ex:
             return jsonify({'success': False, 'message': ex.args}), 401   
 
     def get_today(self):
         try:
-            return jsonify(self.load_json()[date.today().weekday()]), 200
+            return jsonify(self.boss_json[date.today().weekday()]), 200
         except Exception as ex:
             return jsonify({'success': False, 'message': ex.args}), 401    
 
     def get_by_day(self, day):
-        try:
-            return jsonify(self.load_json()[int(day)]), 200
+        try:            
+                            
+            return jsonify(self.boss_json[int(day) - 1]), 200
         except Exception as ex:
             return jsonify({'success': False, 'message': ex.args}), 401    
 
